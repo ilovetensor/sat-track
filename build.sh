758a39063b4c41cc36fa45246979305bfd693d6a
ilovetensor <rohanshar11@gmail.com>
2023-07-31 19:26:15 +0530

    superuser automatic

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 pip install --upgrade pip
 pip3 install -r requirements.txt
 python manage.py collectstatic --no-input
-
+python manage.py createsuperuser --no-input
 python manage.py migrate 
 echo "BUILD END"

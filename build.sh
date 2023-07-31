5d6fa1d2c47794399d421d7876f07c150643bef6
ilovetensor <rohanshar11@gmail.com>
2023-07-31 20:05:58 +0530

    err3

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ set -o errexit
 pip install --upgrade pip
 pip3 install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate 
 echo "BUILD END"

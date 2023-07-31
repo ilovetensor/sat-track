488fd723f8ec73d5d312c5783b82246d9e9924ec
ilovetensor <rohanshar11@gmail.com>
2023-07-31 19:39:41 +0530

    admin site register

--- satTrack/admin.py
+++ satTrack/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Satellite, Sensor
+# Register your models here.
 
 # Register your models here.
+class SatelliteAdmin(admin.ModelAdmin):
+  list_display = ("name", "norad_id", "launch_date","launch_site","status","orbit","swath",)
+
+
+admin.site.register(Satellite, SatelliteAdmin)
+admin.site.register(Sensor)

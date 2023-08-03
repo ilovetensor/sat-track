fb4fdf8fcb5917a110481ef897077a549ada5357
ilovetensor <rohanshar11@gmail.com>
2023-08-03 19:23:04 +0530

    removed tle

--- satTrack/models.py
+++ satTrack/models.py
@@ -68,22 +68,22 @@ class Satellite(models.Model):
         for obj in query_set[:items]:
             obj.delete()
 
-    def save_new_tle(self):
-        query_set = self.tle_set.all()
-        total_tles = len(query_set)
-        if total_tles >= self.max_saved_tle:
-            items = total_tles - self.max_saved_tle+1
-            self.remove_from_set(query_set, items)
+    # def save_new_tle(self):
+    #     query_set = self.tle_set.all()
+    #     total_tles = len(query_set)
+    #     if total_tles >= self.max_saved_tle:
+    #         items = total_tles - self.max_saved_tle+1
+    #         self.remove_from_set(query_set, items)
 
-        tle_fetched = get_tle_from_n2yo(self.norad_id)
-        if tle_fetched == self.tle_now:
-            print("TLE NOT UPDATED NOW !")
-            return
-        else:
-            object = TLE(satellite=self, tle = tle_fetched, date_added = datetime.now())
-            object.save()
-            self.tle_now = tle_fetched
-            self.last_tle_update = datetime.now()
+    #     tle_fetched = get_tle_from_n2yo(self.norad_id)
+    #     if tle_fetched == self.tle_now:
+    #         print("TLE NOT UPDATED NOW !")
+    #         return
+    #     else:
+    #         object = TLE(satellite=self, tle = tle_fetched, date_added = datetime.now())
+    #         object.save()
+    #         self.tle_now = tle_fetched
+    #         self.last_tle_update = datetime.now()
         
 
     def __str__(self):
@@ -91,16 +91,16 @@ class Satellite(models.Model):
     
     
     def save(self, *args, **kwargs):
-        self.save_new_tle()
+        # self.save_new_tle()
         super().save(*args, **kwargs)
     
 
 class TLE(models.Model):
-    satellite = models.ForeignKey(to=Satellite, on_delete=models.CASCADE , editable=True, default={'name': ''})
-    tle = models.TextField("Satellite Tle" ,max_length=100, default='', null=True, editable=True)
-    date_added = models.DateTimeField('Fech datetime ', default=datetime(2000,2,2,2,2,2,2), null=True, editable=True)
+#     satellite = models.ForeignKey(to=Satellite, on_delete=models.CASCADE , editable=True, default={'norad_id': 0000,'name': ''})
+    tle = models.TextField("Satellite Tle" ,max_length=100, default='s', null=True, editable=True)
+#     date_added = models.DateTimeField('Fech datetime ', default=datetime(2000,2,2,2,2,2,2), null=True, editable=True)
 
-    def __str__(self):
-        return f"{self.satellite.name} on {self.date_added.date()} at {self.date_added.strftime('%H:%M:%S')}"
+#     def __str__(self):
+#         return f"{self.satellite.name} on {self.date_added.date()} at {self.date_added.strftime('%H:%M:%S')}"
 
    

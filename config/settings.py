c4674b6e0f12dd562e56aa556fd3399a092f45a8
ilovetensor <rohanshar11@gmail.com>
2023-08-03 20:20:00 +0530

    fix: static files not accessed3

--- config/settings.py
+++ config/settings.py
@@ -135,4 +135,4 @@ STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),

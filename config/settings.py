98c8fe7effeb0b148999a7d5762fecc9f0cd1b1a
ilovetensor <rohanshar11@gmail.com>
2023-08-04 22:17:34 +0530

    update: changes in home page

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
